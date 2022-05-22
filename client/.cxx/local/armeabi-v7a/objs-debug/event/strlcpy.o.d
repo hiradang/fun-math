9d8a8bd00d54811f53234e9636c4a3afde2e0030
hiradang <sunnybinhdang@gmail.com>
2022-05-22 22:38:05 +0700

    delete trash folder

--- client/.cxx/local/armeabi-v7a/objs-debug/event/strlcpy.o.d
+++
@@ -1,8 +0,0 @@
-C:\Users\DELL\dev\fun-math\client\.cxx/local/armeabi-v7a/objs-debug/event/strlcpy.o: \
-  C:\Users\DELL\dev\fun-math\client\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\strlcpy.c \
-  C:\Users\DELL\dev\fun-math\client\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h \
-  C:\Users\DELL\dev\fun-math\client\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h
-
-C:\Users\DELL\dev\fun-math\client\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\include\event2\event-config.h:
-
-C:\Users\DELL\dev\fun-math\client\node_modules\react-native\ReactAndroid\build\third-party-ndk\libevent\evconfig-private.h:
